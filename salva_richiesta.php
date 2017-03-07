4481d3e1cf5150c4b14d97467cbb75d6edcd8b31
Luca Scalvi <luca@scalvi.it>
2017-03-07 18:47:46 +0100

    Aggiunta query per salvare le visure

--- salva_richiesta.php
+++ salva_richiesta.php
@@ -8,5 +8,13 @@ if (isset($_POST['dati'])){
       echo "Dati disponibili: ". $dati."<br>";
       echo "Messaggio: ". $messaggio;
   }
+  $host = 'localhost';
+	$user = 'root';
+	$password = 'audia3';
+	$database = 'certonline';
+ //Aggiunto commento per prova
+$con = mysqli_connect($host, $user, $password, $database);
 
+mysqli_query($con, "INSERT INTO visure (catasto, dati, messaggio, id_cliente) VALUES ('$catasto', '$dati', '$messaggio', 0)");
+mysqli_close($con);
  ?>
